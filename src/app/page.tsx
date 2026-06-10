61acf473109bdd6b07c08333101db3f309966e13
GABRIEL LOPES <frangabriel566@icloud.com>
2026-06-10 17:03:55 -0300

    auto: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ export default function Home() {
         <Benefits />
         <WhyChoose />
         <Coverage />
+        <Offices />
         <LoyaltyContract />
         <Testimonials />
         <FAQ />
