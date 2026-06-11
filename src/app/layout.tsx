09222ca33b2fc03fafb6c4e35e8df7339d3c5587
GABRIEL LOPES <frangabriel566@icloud.com>
2026-06-10 21:20:34 -0300

    auto: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {children}
+        <CityProvider>
+          {children}
+          <CitySelectModal />
+        </CityProvider>
 
         {/* Google Tag Manager */}
         {integrations.googleTagManagerId && (
