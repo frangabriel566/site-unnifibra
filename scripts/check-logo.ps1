2f6c93a065bc3b3c4200ba3f22b8cfd7bdbd608c
GABRIEL LOPES <frangabriel566@icloud.com>
2026-06-10 22:52:57 -0300

    auto: update

--- scripts/check-logo.ps1
+++ scripts/check-logo.ps1
@@ -1,9 +1,8 @@
 Add-Type -AssemblyName System.Drawing
-$img = [System.Drawing.Image]::FromFile((Join-Path $PSScriptRoot "..\public\logo.png"))
+$img = [System.Drawing.Image]::FromFile((Join-Path $PSScriptRoot "..\public\logo.png.transparent.png"))
+Write-Output "$($img.Width)x$($img.Height) $($img.PixelFormat)"
 $bmp = [System.Drawing.Bitmap]$img
-$w = $bmp.Width
-$h = $bmp.Height
-foreach ($pt in @(@(0,0), @(5,5), @(0,627), @(627,0), @(1253,1253), @(1253,0), @(0,1253), @(50,50), @(627,627))) {
+foreach ($pt in @(@(0,0), @(5,5), @(627,0), @(1253,1253), @(627,627), @(627,1100))) {
     $c = $bmp.GetPixel($pt[0], $pt[1])
-    Write-Output "($($pt[0]),$($pt[1])): R=$($c.R) G=$($c.G) B=$($c.B)"
+    Write-Output "($($pt[0]),$($pt[1])): A=$($c.A) R=$($c.R) G=$($c.G) B=$($c.B)"
 }
