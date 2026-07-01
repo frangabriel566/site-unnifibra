0389ad4e469b6685e4a1acdce1e51938e1708013
GABRIEL LOPES <frangabriel566@icloud.com>
2026-07-01 13:04:53 -0300

    auto: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,12 @@ const geistMono = Geist_Mono({
 
 export const dynamic = "force-dynamic";
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 5,
+};
+
 export async function generateMetadata(): Promise<Metadata> {
   const siteConfig = await getMergedSiteConfig();
 
